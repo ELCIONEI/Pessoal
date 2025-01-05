0360a718267ce377bede90fa8176471c20aeefe8
elcionei <elcioneifernandes@gmail.com>
2025-01-05 12:06:56 -0300

    atualizado

--- medidorCo2.py
+++ medidorCo2.py
@@ -9,15 +9,16 @@
 print('********** SISTEMA DE MEDIÇÃO DE cO2 ATMOSFERICO **********')
 niveis_cO2 = {
     'Planaltina': [330, 440, 550, 498, 650], 
-    'Paranoar':   [456, 650, 440, 350, 498],
+    'Paranoa':   [456, 650, 440, 350, 498],
     'Esplanada':  [350, 386, 456, 654, 356],
     'Ceilandia':  [123, 456, 654, 489, 455],
+    'Sobradinho': [123, 321, 345, 543, 542],
     }
 for cidade in niveis_cO2:
     qtde_sensores = len(niveis_cO2[cidade])
     total_cO2 = sum(niveis_cO2[cidade])
     media_cO2 = total_cO2 / qtde_sensores
     if media_cO2 > 450:
-        print('A cidade de: {} está com nivel de cO2 de: {} :!')
+        print('A cidade de: {} está com nivel de cO2 de: {} :!'.format(cidade, media_cO2))
     else:
         pass
